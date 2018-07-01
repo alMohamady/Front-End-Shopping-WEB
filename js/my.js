482201b116c0d4943c6c483052dffc508cc2fdb6
alMohamady <al.Mohamady47@gmail.com>
2018-07-01 15:55:21 +0300

    Make responsive navigation bar

--- js/my.js
+++ js/my.js
@@ -2,5 +2,11 @@ $(document).ready(function(){
 	$('.bxslider').bxSlider({
 		mode: 'horizontal',
 		auto: true,
+	});
+	
+	$('.menu-togglr').on('click',function(){
+    $('#mainav').slideToggle('fast');
+    $(this).toggleClass('active');
   });
+
 });
